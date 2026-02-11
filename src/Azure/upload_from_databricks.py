09b410f46dd8682550c26081181bde2331ce72d3
dhthink <davehaloski@gmail.com>
2026-02-10 23:39:55 -0500

    Update storage account key placeholder in upload script

--- src/Azure/upload_from_databricks.py
+++ src/Azure/upload_from_databricks.py
@@ -6,7 +6,7 @@ from azure.storage.blob import BlobServiceClient
 # Azure storage
 DATABRICKS_FILE_URL = "https://dbc-a20fb33f-459f.cloud.databricks.com/explore/data/workspace/onedrive/ev_raw_csv?o=875246317280872"  # your full URL
 STORAGE_ACCOUNT_NAME = "entdatalake01ev"  # or your exact account name
-STORAGE_ACCOUNT_KEY = "Uour key code"
+STORAGE_ACCOUNT_KEY = "Your key code"
 CONTAINER_NAME = "ev_raw_csvev-data"
 BLOB_NAME = "ev_data.csv"  # or "raw/ev_data.csv" if you want a folder-like path
 # ========================
